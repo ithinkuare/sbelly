43a3ecfd1ff1d933bd74508ebb9d6fc219a3d071
user <user@www.soundbelly.com>
2011-09-03 23:26:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sb::Application.config.secret_token = 'a0073ea2c5c6e10df77890b93fda027a0b4b464c8262a518f6f4485d894ba96ffa2d243ce9c82933f8035a993c6dd3140243cf757a73f0eea1b7e7fdc447e9b0'
